b0f04b16ef25a2d33a53841123a803a32c62bb21
ChipsInChief <chipsinchief@gmail.com>
2024-03-07 22:14:58 +0100

    🚀 Séance #02 : Première version du projet

---
+++ secret-santa.py
@@ -0,0 +1,30 @@
+# Définition de quelques participants
+# Utilisation de variables pour stocker des informations simples
+participant1_name = "Alice"
+participant1_email = "alice@example.com"
+
+participant2_name = "Bob"
+participant2_email = "bob@example.com"
+
+# Calcul simple pour montrer l'utilisation des types numériques
+number_of_participants = 2
+print("Nombre de participants :")
+print(number_of_participants)
+
+# Affichage des informations des participants
+print("--------------- PARTICIPANTS ---------------")
+print(participant1_name)
+print(participant1_email)
+
+print(participant2_name)
+print(participant2_email)
+print("--------------------------------------------")
+
+# Assignation manuelle
+cadeau_de_Alice = "Bob"  # Alice offre un cadeau à Bob
+cadeau_de_Bob = "Alice"  # Bob offre un cadeau à Alice
+
+# Affichage des paires Secret Santa
+print("Résultat du tirage :")
+print(participant1_name, "offre un cadeau à", cadeau_de_Alice)
+print(participant2_name, "offre un cadeau à", cadeau_de_Bob)
